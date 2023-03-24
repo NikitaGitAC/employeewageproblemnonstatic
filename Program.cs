9b1ccfd8f6c9877fc463656ecaabc5b24d547221
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-24 19:15:59 +0530

    greeting msg

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using static Employeewage_problemoop.empcheck;
+
+namespace Employeewage_problemoop
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation problem");
+            EmpAttendence Attendance = new EmpAttendence();
+            Attendance.EmployeeCheck();
+        }
+    }
+}
